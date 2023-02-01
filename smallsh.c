3d69b063081c6f949abfd59ec307d98f67b54b4b
tylersnel <67352917+tylersnel@users.noreply.github.com>
2023-02-01 12:13:55 -0700

    Working on parsing
    Still trying to work out the kinks on parsing

--- smallsh.c
+++ smallsh.c
@@ -13,8 +13,20 @@
 #include <string.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 char *search_replace(char *restrict *restrict haystack, char const *restrict needle, char const *restrict sub);
+int parser(char **copies);
+
+//global variables for parser
+int pound_loc = -1;
+int ampersand_loc = -1;
+int less_loc = -1;
+int less_token = -1;
+int greater_loc = -1;
+int greater_token = -1;
 
 int main(int argc, char *argv[])
   {
@@ -54,17 +66,37 @@ int main(int argc, char *argv[])
         token = strtok(NULL, ifs);
         i++;
       }
+      /*
+       * Used of expansion
+       * Two for loops, to check each occurance with each word.
+       * Calls search_replace and replaces copies in word_copies
+       * if expansion needed.
+       */
+      char *home_get=getenv("HOME");
+      strcat(home_get,"/");
+      
+      int pid_t=getpid();
+      char pid_buffer[50];
+      sprintf(pid_buffer,"%d", pid_t);
+      
+      int child_status;
+      waitpid(pid_t, &child_status, 0);
+      char status_buffer[10];
+      sprintf(status_buffer, "%d", child_status);
+
+      char *needle[]={"~/", "$$", "$?", "$!"};
+      char *sub[]={home_get, pid_buffer, status_buffer, "holder"};
       for (int j = 0; j<i; j++){
-        char *needle[]={"~/", "$$", "$?", "$!"};
         for (int needle_count =0; needle_count < 4; needle_count++){
-          char *ret = search_replace(&word_copies[j], needle[needle_count], "p");
+          char *ret = search_replace(&word_copies[j], needle[needle_count], sub[needle_count]);
         }
       }
-      int w=0;
-      while(word_copies[w]){
-        printf("%s\n", word_copies[w]);
-        w++;
-      }
+     // int w=0;
+      //while(word_copies[w]){
+        //printf("%s ", word_copies[w]);
+        //w++;
+      //}
+    parser(word_copies);
    }    
 
   free(line);
@@ -104,4 +136,45 @@ char *search_replace(char *restrict *restrict haystack, char const *restrict nee
 
 exit:
     return str;
+  }
+
+int parser(char **copies)
+  { 
+    int i=0;
+    while(copies[i]){
+        if (strcmp(copies[i], "#") == 0) pound_loc=i;
+        i++;
+      }
+    if(pound_loc<0) pound_loc=i;//if end of array and not # hit
+    
+    i=pound_loc;
+    if ((i-2) >= 0){
+    
+      if (strcmp(copies[i-1], "&") == 0){// if ampersand, that index point becomes the check for < amd > this i decreased to match apmersand 
+        ampersand_loc=i-1;
+        i=i-1;
+      }
+      if(strcmp(copies[i-2],"<")==0){
+        less_loc = i-2;
+        less_token = i-1;
+      }
+      if(strcmp(copies[i-2],">")==0){
+        greater_loc = i-2;
+        greater_token = i-1;
+      }
+    }
+    if ((i-4) >= 0){
+    
+      if(strcmp(copies[i-4],"<")==0){
+        less_loc = i-4;
+        less_token = i-3;
+      }
+      if(strcmp(copies[i-4],">")==0){
+        greater_loc = i-4;
+        greater_token = i-3;
+      }
+    }
+    
+   printf("#=%d, &=%d, < = %d, less_tock = %d, >=%d, greater_toc=%d",pound_loc, ampersand_loc, less_loc, less_token, greater_loc, greater_token);
+   return 0;
   }
